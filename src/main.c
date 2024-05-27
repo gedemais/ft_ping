23f1d59d0e4f39cb775b7e01642148438cbca1c7
gedemais <vente.ampoules@gmail.com>
2024-05-27 22:54:04 +0200

    root bypass added

--- src/main.c
+++ src/main.c
@@ -3,51 +3,51 @@
 void recv_ping(int sockfd, int seq_num, struct timeval *tv_in, struct options *opts)
 {
 	//printf("----- %s -----\n", __FUNCTION__);
-    struct timeval tv_out;
-    struct sockaddr_in recv_addr;
+	struct timeval tv_out;
+	struct sockaddr_in recv_addr;
 	struct stats	*stats;
-    socklen_t addr_len = sizeof(recv_addr);
-    char recv_buf[MAX_PACKET_SIZE];
-    ssize_t recv_size;
+	socklen_t addr_len = sizeof(recv_addr);
+	char recv_buf[MAX_PACKET_SIZE];
+	ssize_t recv_size;
 
 	stats = stats_singleton();
-    gettimeofday(&tv_out, NULL);
+	gettimeofday(&tv_out, NULL);
 
-    while (1) {
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = opts->timeout * 1000;
-        fd_set readfds;
-        FD_ZERO(&readfds);
-        FD_SET(sockfd, &readfds);
+	while (1) {
+		struct timeval timeout;
+		timeout.tv_sec = 0;
+		timeout.tv_usec = opts->timeout * 1000;
+		fd_set readfds;
+		FD_ZERO(&readfds);
+		FD_SET(sockfd, &readfds);
 
-        int ready = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
-        if (ready < 0) {
-            perror("select");
-            exit(EXIT_FAILURE);
-        } else if (ready == 0) {
-            //printf("Request timed out for sequence %d\n", seq_num);
-            return;
-        }
+		int ready = select(sockfd + 1, &readfds, NULL, NULL, &timeout);
+		if (ready < 0) {
+			perror("select");
+			exit(EXIT_FAILURE);
+		} else if (ready == 0) {
+			//printf("Request timed out for sequence %d\n", seq_num);
+			return;
+		}
 
-        recv_size = recvfrom(sockfd, recv_buf, sizeof(recv_buf), 0, (struct sockaddr *)&recv_addr, &addr_len);
-        if (recv_size <= 0) {
-            perror("recvfrom");
-            exit(EXIT_FAILURE);
-        }
+		recv_size = recvfrom(sockfd, recv_buf, sizeof(recv_buf), 0, (struct sockaddr *)&recv_addr, &addr_len);
+		if (recv_size <= 0) {
+			perror("recvfrom");
+			exit(EXIT_FAILURE);
+		}
 
-        struct iphdr *ip_hdr = (struct iphdr *)recv_buf;
-        struct icmp *icmp_hdr = (struct icmp *)(recv_buf + (ip_hdr->ihl << 2));
+		struct iphdr *ip_hdr = (struct iphdr *)recv_buf;
+		struct icmp *icmp_hdr = (struct icmp *)(recv_buf + (ip_hdr->ihl << 2));
 
-        if (icmp_hdr->icmp_type == ICMP_ECHOREPLY && icmp_hdr->icmp_id == getpid() && icmp_hdr->icmp_seq == seq_num) {
-            gettimeofday(tv_in, NULL);
-            double rtt = (tv_in->tv_sec - tv_out.tv_sec) * 1000.0 + (tv_in->tv_usec - tv_out.tv_usec) / 1000.0;
-            printf("%ld bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n", recv_size - sizeof(struct iphdr), inet_ntoa(recv_addr.sin_addr), seq_num, ip_hdr->ttl, rtt);
+		if (icmp_hdr->icmp_type == ICMP_ECHOREPLY && icmp_hdr->icmp_id == getpid() && icmp_hdr->icmp_seq == seq_num) {
+			gettimeofday(tv_in, NULL);
+			double rtt = (tv_in->tv_sec - tv_out.tv_sec) * 1000.0 + (tv_in->tv_usec - tv_out.tv_usec) / 1000.0;
+			printf("%ld bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n", recv_size - sizeof(struct iphdr), inet_ntoa(recv_addr.sin_addr), seq_num, ip_hdr->ttl, rtt);
 			stats->received_packets++;
 			fflush(stdout);
-            return;
-        }
-    }
+			return;
+		}
+	}
 }
 
 void handle_sigint(int sig) {
@@ -56,33 +56,61 @@ void handle_sigint(int sig) {
 
 	stats = stats_singleton();
 	display_stats(stats);
-    exit(0);
+	exit(0);
+}
+
+int	open_raw_socket(int *sockfd)
+{
+	struct protoent *proto = getprotobyname ("icmp");
+
+	*sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (*sockfd < 0)
+	{
+		if (errno == EPERM || errno == EACCES)
+		{
+			errno = 0;
+
+			*sockfd = socket (AF_INET, SOCK_DGRAM, proto->p_proto);
+			if (*sockfd < 0)
+			{
+				if (errno == EPERM || errno == EACCES || errno == EPROTONOSUPPORT)
+					fprintf (stderr, "ping: Lacking privilege for icmp socket.\n");
+				else
+					fprintf (stderr, "ping: %s\n", strerror(errno));
+
+				return (-1);
+			}
+		}
+		else
+			return (-1);
+	}
+	return (0);
 }
 
 
 int main(int argc, char *argv[]) {
-    struct		options opts = {0};
+	struct		options opts = {0};
 	struct		stats *stats;
-    char		*target = NULL;
+	char		*target = NULL;
 	char		address[INET_ADDRSTRLEN];
 	char		domain_name[NI_MAXHOST];
 	const int	pid = getpid();
 
-    memset(&opts, 0, sizeof(struct options));
-    opts.packet_size = 64; // Default packet size
+	memset(&opts, 0, sizeof(struct options));
+	opts.packet_size = 64; // Default packet size
 	opts.timeout = 1000; // Default timeout
 	opts.interval = FLT_MAX; // Default interval
-    parse_args(argc, argv, &opts, &target);
+	parse_args(argc, argv, &opts, &target);
 
-    int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-    if (sockfd < 0) {
-        perror("socket");
-        exit(EXIT_FAILURE);
-    }
+	int sockfd = open_raw_socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+	if (sockfd < 0) {
+		perror("socket");
+		exit(EXIT_FAILURE);
+	}
 
-    struct sockaddr_in dest_addr;
-    memset(&dest_addr, 0, sizeof(dest_addr));
-    dest_addr.sin_family = AF_INET;
+	struct sockaddr_in dest_addr;
+	memset(&dest_addr, 0, sizeof(dest_addr));
+	dest_addr.sin_family = AF_INET;
 
 	if (get_hostname_address(target, address) < 0)
 		return (1);
@@ -90,12 +118,12 @@ int main(int argc, char *argv[]) {
 	if (get_domain_name(address, domain_name) < 0)
 		return (1);
 
-    int s = inet_pton(AF_INET, address, &dest_addr.sin_addr);
-    if (s <= 0) {
-        exit(EXIT_FAILURE);
-    }
+	int s = inet_pton(AF_INET, address, &dest_addr.sin_addr);
+	if (s <= 0) {
+		exit(EXIT_FAILURE);
+	}
 
-    printf("PING %s (%s): %ld data bytes", target, address, opts.packet_size - sizeof(struct icmphdr));
+	printf("PING %s (%s): %ld data bytes", target, address, opts.packet_size - sizeof(struct icmphdr));
 	if (opts.verbose == 1)
 		printf(", id 0x%04x = %d\n", pid, pid);
 	else
@@ -117,8 +145,8 @@ int main(int argc, char *argv[]) {
 		i++;
 	}
 
-    close(sockfd);
+	close(sockfd);
 
 	display_stats(stats);
-    return 0;
+	return 0;
 }
